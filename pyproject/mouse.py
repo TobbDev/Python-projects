9599ee39379a6f68570bc54358c6638555325bb7
TobDev <77089564+TobbDev@users.noreply.github.com>
2022-04-14 21:22:21 +0700

    Create A New File 'mouse.py'

---
+++ pyproject/mouse.py
@@ -0,0 +1,13 @@
+import mouse
+
+mouse.move(577,735,True,2)
+mouse.click('left')
+mouse.move(929,670,True,2)
+mouse.click('left')
+mouse.move(925,567,True,1.5)
+mouse.click('left') 
+mouse.click('left') 
+
+# (529, 743) ==> windows icon
+# (936, 672) ==> power icon
+# (935, 598) ==> shutdown icon
